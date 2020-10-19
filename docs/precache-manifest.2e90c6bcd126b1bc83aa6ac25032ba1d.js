50a55c036ec4accee921474ba5832eb4487cf06e
dj_skribble_16@hotmail.com <jsepulveda@elektra.com.mx>
2020-10-19 11:03:52 -0500

    Docs agregado

---
+++ docs/precache-manifest.2e90c6bcd126b1bc83aa6ac25032ba1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa7eb1284d38f73c560a0da2af947bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e77a9e1b1befaf8732e9",
+    "url": "/static/css/main.413c259e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77a9e1b1befaf8732e9",
+    "url": "/static/js/main.ff2020c4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
